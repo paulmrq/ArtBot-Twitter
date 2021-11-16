d75c8535bee528c150832aefcb24d42048e0956b
paulmrq <marquereau.paul@gmail.com>
2021-11-16 17:04:04 +0100

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -18,26 +18,23 @@ date = file['images'][0]['date']
 
 message = f"{name} by {author},{date}"
 api = connect_twitter_api()
-#media = api.media_upload(f"images/{image_ID}.jpg")
+media = api.media_upload(f"images/{image_ID}.jpg")
 
-#api.update_status(status = message,media_ids=[media.media_id])
+api.update_status(status = message,media_ids=[media.media_id])
 
 
 
-drive = connect_google_api()
 
-# View all folders and file in your Google Drive
-fileList = drive.ListFile({'q': "'1LeLvu0UDJyHf9WjIlk_pbQlBXGbsWqUS' in parents and trashed=false"}).GetList()
-for file in fileList:
-  print('Title: %s, ID: %s' % (file['title'], file['id']))
+def gdrive_method():
 
-  # Get the folder ID that you want
-  if(file['title'] == "To Share"):
-      fileID = file['id']
+    drive = connect_google_api()
 
-#file2 = drive.CreateFile({'id': fileID})
+    # View all folders and file in your Google Drive
+    fileList = drive.ListFile({'q': "'1LeLvu0UDJyHf9WjIlk_pbQlBXGbsWqUS' in parents and trashed=false"}).GetList()
+    for file in fileList:
+      print('Title: %s, ID: %s' % (file['title'], file['id']))
 
-media = api.media_upload(file)
-print(media)
-#api.update_status(status = message,media_ids=[media.media_id])
+      # Get the folder ID that you want
+      if(file['title'] == "To Share"):
+          fileID = file['id']
 
